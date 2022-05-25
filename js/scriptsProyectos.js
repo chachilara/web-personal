353930c52252cdd0de6ff9a8aca97b9e174dd953
lara <laradmm4@gmailcom>
2022-05-25 15:59:56 +0200

    Añadiendo estilos a mi web

---
+++ js/scriptsProyectos.js
@@ -0,0 +1,11 @@
+<script>
+        function changeImage() {
+            var image = document.getElementById('Hercio');
+            if (image.src.match("Hercio")) {
+                image.src = "../img/HercioNuevoEditado8.jpg";
+            }
+            else {
+                image.src = "../img/Hercio_Logo.jpg";
+            }
+        }
+    </script>
